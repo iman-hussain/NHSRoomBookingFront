d60dac99716e64ca1ddb5fafd9d1edbc00da8ad0
LiamCSDev <57903273+LiamCSDev@users.noreply.github.com>
2020-02-20 13:10:25 +0000

    Updating Calendar.Js
    Updated the Calendar component to enable switching between months, also works with previewing previous and future years.
    Current issue to solve is showing either 5 or 6 rows depending on the first day of the month and the total days in the month.

--- src/Pages/Calendar/Calendar.js
+++ src/Pages/Calendar/Calendar.js
@@ -4,71 +4,138 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Decemeber"];
 let DaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-let DisplayMonth = 0;
-let DisplayYear = 0;
-/*
-TODO - Display Year - Month - Current Day - Leap Year - Next / Previous button
-*/
 
-/* Returns the Current Year - Used for start up of calendar*/
-function currentYear(){
-  DisplayYear = new Date().getFullYear();
-}
+class App extends Component {
 
-/* Returns the Current Month - Used for start up of calendar*/
-function currentMonth(){
- DisplayMonth = new Date().getMonth();
-}
-
-function setUp(){
-  currentYear()
-  currentMonth()
-}
-
-/* Determines how many rows will be needed for the calendar
-Default 5: 
-Six if First day of the month is: 
-Thursday & days in month is 31 or
-Friday & days in month is >=30 or
-Saturday & days in month is > 29
-*/
-function calendarRows(){
-  return 5;
-}
-
-/*Change the amount of days shown in February if the currently viewed year is a leap year.*/
-function checkLeapYear(){
-  let date = new Date(), y = date.getFullYear(), m = date.getMonth();
-  let leapYear = m === 1 && y % 4 === 0;
-  if (leapYear) {
-    DaysInMonth[1] = 29;
-  } else {
-    DaysInMonth[1] = 28;
+  constructor(props) {
+    super(props);
+    this.state = {
+      month: 0,
+      year: 0,
+      leapYear: false,
+      calendarRows: 5,
+      dates: [],
+      content: [],
+    };
+    // Bind Functions to button press.
+    this.nextMonth = this.nextMonth.bind(this);
+    this.previousMonth = this.previousMonth.bind(this);
   }
-}
 
-/*Used to move the calendar forward by a month*/
-function nextMonth(){
-  DisplayMonth += 1;
-  console.log(DisplayMonth);
-}
+  //Functions Called On Load
+  componentDidMount(){
+    this.currentYear()
+    this.currentMonth()
+    this.checkLeapYear()
+    this.calendarRows()
+  }
 
-/*Used to move the calendar back by a month*/
-function previousMonth(){
-  DisplayMonth -= 1;
-  console.log(DisplayMonth);
-}
+  //Used on button press to load in the next month
+  nextMonth(){
+    this.checkLeapYear()
+    this.setState({
+      month: this.state.month > 10 ? 0 : this.state.month + 1,
+      year: this.state.month > 10 ? this.state.year + 1 : this.state.year,
+      calendarRows: this.nextMonthCheck() ? 6 : 5
+    });
+  }
+  
+  // Used on button press to load in the previous month
+  previousMonth(){
+    this.checkLeapYear()
+    this.setState({
+      month: this.state.month < 1 ? 11 : this.state.month - 1,
+      year: this.state.month < 1 ? this.state.year - 1 : this.state.year,
+      calendarRows: this.prevMonthCheck() ? 6 : 5
+    });
+  }
 
-function App(){
-  setUp();
+  // Check the previous month values, to determine how many rows need to be shown on the calendar
+  prevMonthCheck(){
+    var checkMonth = this.state.month < 1 ? 11 : this.state.month - 1
+    var checkYear = this.state.month < 1 ? this.state.year - 1 : this.state.year
+    var firstDay = (new Date(checkYear, checkMonth)).getDay()
+    return (firstDay === 5 && DaysInMonth[checkMonth] >= 30) || 
+    (firstDay === 6 && DaysInMonth[checkMonth] > 29)
+  }
+
+  // Check the next month value, to determine how many rows need to be shown on the calendar
+  nextMonthCheck(){
+    var checkMonth = this.state.month > 10 ? 0 : this.state.month + 1;
+    var checkYear = this.state.month > 10 ? this.state.year - 1 : this.state.year
+    var firstDay = (new Date(checkYear, checkMonth)).getDay()
+    return (firstDay === 5 && DaysInMonth[checkMonth] >= 30) || 
+    (firstDay === 6 && DaysInMonth[checkMonth] > 29)
+  }
+
+  // Get the current year
+  currentYear(){
+    this.setState({
+      year: new Date().getFullYear()
+    });
+  }
+
+  // Get the current month
+  currentMonth(){
+    this.setState({
+      month: new Date().getMonth()
+    });
+  }
+  
+  // Check if the viewing year is a leap year.
+  checkLeapYear(){
+    this.setState({
+      leapYear: this.state.year % 4 === 0 ? true : false
+    });
+  }
+
+  /* Determines how many rows will be needed for the calendar
+  Default 5: 
+  Six if First day of the month is: 
+  Friday & days in month is >=30 or
+  Saturday & days in month is > 29
+
+  Or Days in Month + FirstDayValue === 35 then 6 Rows
+  Or RoundUp((Days in Month + FirstDayValue + 1) / 7)
+  */
+  calendarRows(){
+    var firstDay = (new Date(this.state.year, this.state.month)).getDay()
+    let date = new Date(this.state.year, this.state.month), m = date.getMonth();
+    console.log("First Day: " + firstDay)
+    console.log("Month: " + m)
+    console.log("Days in Month: " + DaysInMonth[m])
+    this.setState({
+      calendarRows: (firstDay === 5 && DaysInMonth[m] >= 30) || (firstDay === 6 && DaysInMonth[m] > 29) ? 6 : 5
+    });
+  }
+
+  // Component to display the calendar.
+  calendarContent(){
+    return [...Array(this.state.calendarRows)].map((e, r) => {
+      var firstDay = (new Date(this.state.year, this.state.month)).getDay();
+      var days = DaysInMonth[this.state.month];
+      if(this.state.leapYear && this.state.month === 1){
+        days = 29;
+      }
+      var day = r * 7;
+      return <tr key={r}>{[...Array(7)].map((e, i) => {
+        day+=1;
+        return <th key={i}>{day <= days + firstDay && day > firstDay ? day - firstDay : ''}
+        <p>{day <= days + firstDay ? "Content" : ''}</p>
+        </th>
+      })}</tr>
+    })
+  }
+
+  render(){
     return(
       <Table striped bordered hover>
         <thead>
           <tr>
             <th colSpan="7">
-              <button onClick={previousMonth}>Previous</button>
-              Calendar - {Months[DisplayMonth]} - {DisplayYear}.
-              <button onClick={nextMonth}>Next</button>
+              <button onClick={this.previousMonth}>Previous</button>
+              Calendar - {Months[this.state.month]} - {this.state.year}.
+              <button onClick={this.nextMonth}>Next</button>
             </th>
           </tr>
         </thead>
@@ -83,70 +150,12 @@ function App(){
             <th>Saturday</th>
           </tr>
         </thead>
-        <tbody>{[...Array(calendarRows())].map((e,r) => {
-          let date = new Date(), y = date.getFullYear(), m = date.getMonth();
-          //let lastDay = new Date(y, m + 1, 0);
-          let firstDay = (new Date(y, m)).getDay();
-          let leapYear = m === 1 && y % 4 === 0;
-          let days = DaysInMonth[m];
-          if (leapYear) {
-            days = 29;
-          }
-          let day = r * 7;
-          return <tr key={r}>{[...Array(7)].map((e, i) => {
-            day += 1;
-              return <th key={i}>{day <= days+firstDay && day > firstDay ? day-firstDay : ''}
-                    <p>{day <= days+firstDay ? "Content" : ''}</p>
-                    </th>
-            })}</tr>
-        })}
+        <tbody>
+          {this.calendarContent()}
         </tbody>
       </Table>
     );
+  }
 }
 
 export default App;
-
-
-/*import React from 'react';
-import Calendar from 'react_google_calendar'
-
-/*
-TODO: Create Calander Component - Connect to Google API
-/*
-
-const calendar_configuration = {
-    api_key: 'AIzaSyBahaMZOI8jFnjLC9SPgLBJqwxNt37vSQk'
-    ,
-    calendars: [
-      {
-        name: 'demo',
-        url: 'exampleURL@group.calendar.google.com'
-      }
-    ],
-    dailyRecurrence: 700,
-    weeklyRecurrence: 500,
-    monthlyRecurrence: 20
-}
-
-class MyApp extends React.Component {
-    constructor(props) {
-      super(props)
-        this.state = {
-          events: []
-        }
-    }
-}
-
-function Calendar2(){
-    return (
-        <div>
-        <Calendar
-          events={this.state.events}
-          config={calendar_configuration} />
-      </div>
-    );
-}
-
-export default Calendar2;
-*/
